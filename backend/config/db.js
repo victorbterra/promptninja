c9339e1b1a4ab2f73a39e4296b87cf203e29b637
Victor Terra <victorterradev@gmail.com>
2026-03-02 18:46:55 -0300

    feat: integracao do banco de dados com supabase

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,23 @@
 import dotenv from 'dotenv';
-import pg from 'pg';
-
+import postgres from 'postgres'
 
 dotenv.config();
 
-const { Pool } = pg;
 
-const dataBase = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,    
-})
 
-dataBase.connect()
-    .then(()=> console.log("✅ Banco de dados conectado com sucesso!"))
-    .catch((erro)=> console.log("❌ Erro ao conectar no banco:", erro.message))
+const connectionString = process.env.DATABASE_URL;
+const sql = postgres(connectionString);
 
-export default dataBase;
+const testarConexao = async () =>{
+    try{
+        await sql`SELECT 1`;
+        console.log("✅ Banco de dados conectado com sucesso na nuvem!")
+    }
+    catch(erro){
+        console.error("❌ Erro ao conectar no banco de dados:", erro.message);
+    }
+}
+
+testarConexao();
+
+export default sql
