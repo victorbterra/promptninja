56f961b02acc109f6d8b87bff795463f29111321
Victor Terra <victorterradev@gmail.com>
2026-03-03 14:42:34 -0300

    feat: adiciona lógica de busca do problema.

--- backend/src/controllers/solucoesController.js
+++ backend/src/controllers/solucoesController.js
@@ -1,11 +1,37 @@
-export const buscarSolucao = (req, res) => {
-    const {sistema, problema } = req.body;
+import sql from '../../config/db.js';
 
-    res.json({
-        mensagem:"Problema recebido com sucesso!",
-        sistemaInformado: sistema,
-        problemaInformado:problema,
-        comandoSugerido:"sfc /scannow",
-        explicacao:"Este comando verifica e corrige arquivos corrompidos no sistema."
-    })
+export const buscarSolucao = async (req, res) => {
+    try{
+        const {sistema, problema } = req.body;
+
+        const termoBusca = `%${problema}%`;
+
+        const resultados = await sql`
+        SELECT
+            c.problema,
+            c.codigo_terminal,
+            c.explicacao
+        FROM comandos c
+        JOIN sistemas s ON c.id_sistema = s.id
+        WHERE
+            s.nome ILIKE ${sistema} AND
+            c.problema ILIKE ${termoBusca}
+        `;
+
+        if(resultados.length === 0) {
+            return res.status(404).json({
+                mensagem:"Não encontramos nenhum comandos para isso.",
+                resultados: []
+            });
+        }
+
+        res.json({
+            mensagem:"Comandos encontrados com sucesso !",
+            totalEncontrado:resultados.length,
+            resultados: resultados
+        })
+    } catch(erro) {
+        console.error("Erro na busca",erro.message);
+        res.status(500).json({erro:"Erro interno do servidor."});
+    }
 }
